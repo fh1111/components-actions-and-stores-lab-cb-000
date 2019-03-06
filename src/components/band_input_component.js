41db01f0d646818c57ec7c6f575e8eab49da5d91
Fer Hernandez <fh.1111@gmail.com>
2019-03-06 02:24:32 +0000

    Automatically backed up by Learn

--- src/components/band_input_component.js
+++ src/components/band_input_component.js
@@ -1,23 +1,25 @@
 import React from 'react';
 
 export default class BandInput extends React.Component {
-
-  onSubmit(){
-    event.preventDefault();
-    console.log("NOMG I'm in teh onSubmit() functions")
+  constructor(props) {
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
   }
 
-    render(){
-        return(
-        <div>
-            <form onSubmit={this.onSubmit.bind(this)}>
-                <label>Enter Band: </label>
-                <input />
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-      )
+  onSubmit(e) {
+    e.preventDefault()
+    this.props.store.dispatch({type: 'ADD_BAND', payload: {title: 'New Band'}})
+  }
 
-    }
-
+  render(){
+    return(
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <label>Enter Band: </label>
+          <input />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    )
+  }
 }
