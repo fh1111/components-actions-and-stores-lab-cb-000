44bfe50921ec7ead519a190e0ed660a006770050
Fer Hernandez <fh.1111@gmail.com>
2019-03-06 02:17:10 +0000

    Automatically backed up by Learn

--- src/components/band_input_component.js
+++ src/components/band_input_component.js
@@ -3,19 +3,20 @@ import React from 'react';
 export default class BandInput extends React.Component {
 
   onSubmit(){
+    event.preventDefault();
     console.log("NOMG I'm in teh onSubmit() functions")
   }
 
     render(){
-      return(
-      <div>
-          <form onSubmit={this.onSubmit}>
-              <label>Enter Band: </label>
-              <input />
-              <button type="submit">Submit</button>
-          </form>
-      </div>
-    )
+        return(
+        <div>
+            <form onSubmit={this.onSubmit}>
+                <label>Enter Band: </label>
+                <input />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+      )
 
     }
 
