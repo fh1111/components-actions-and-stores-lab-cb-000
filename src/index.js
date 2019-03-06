b9f1ac79369133593d49c27e907ce51c58f1438c
Fer Hernandez <fh.1111@gmail.com>
2019-03-06 02:10:27 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -9,5 +9,5 @@ import BandInput from './components/band_input_component';
 const store = createStore(reducer)
 
 function render(){
-  ReactDOM.render(<BandInput />, document.getElementById('container'))
+  ReactDOM.render(<BandInput store={store} />, document.getElementById('container'))
 }
